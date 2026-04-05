62816e34091b65bcf2cd975ec5ba76d6aa78e80f
diguwu <digbijoyat2005@gmail.com>
2026-04-05 22:05:52 +0530

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Oceandea from './Oceandea';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Oceandea />
+    </div>
+  );
+}
+
+export default App;
